6b6c02b0580e067967ac74316b14118c3986ab56
Charlelie BOUSSAUD <charlelie.boussaud@hotmail.fr>
2017-09-05 17:36:48 +0200

    dr quine is done with its bonus

---
+++ bonus/src/Colleen.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ 	OUTSIDE COMMENT
+*/
+void useless_function(){}
+int main(){
+/*
+ 	INSIDE COMMENT
+*/
+useless_function();
+const char *s="#include <stdio.h>%1$c/*%1$c%2$cOUTSIDE COMMENT%1$c*/%1$cvoid useless_function(){}%1$cint main(){%1$c/*%1$c%2$cINSIDE COMMENT%1$c*/%1$cuseless_function();%1$cconst char *s=%3$c%s%3$c;%1$cprintf(s,10,9,34,s);}";
+printf(s,10,9,34,s);}
---
+++ bonus/src/Grace.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <fcntl.h>
+#define WRITE(fd,s) dprintf(fd,s,10,9,34,s)
+#define OPEN() open("Grace_kid.cpp",O_CREAT|O_WRONLY,0644)
+#define MAIN(s) int main(){WRITE(OPEN(),s);}
+/*
+	COMMENT
+*/
+MAIN("#include <stdio.h>%1$c#include <fcntl.h>%1$c#define WRITE(fd,s) dprintf(fd,s,10,9,34,s)%1$c#define OPEN() open(%3$cGrace_kid.cpp%3$c,O_CREAT|O_WRONLY,0644)%1$c#define MAIN(s) int main(){WRITE(OPEN(),s);}%1$c/*%1$c%2$cCOMMENT%1$c*/%1$cMAIN(%3$c%4$s%3$c)")
